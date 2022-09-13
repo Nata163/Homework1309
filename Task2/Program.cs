f915f53f2a537f6108342fd55acfae49c5eac320
Nata <nata.9688@mail.ru>
2022-09-13 16:38:09 +0400

    Add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigit(int num)
+{
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+
+try
+{
+    int number = int.Parse(Console.ReadLine());
+    int numberModule = Math.Abs(number);
+
+    int result = SumDigit(numberModule);
+    Console.WriteLine($"Сумма цифр в числе {number} равна {result}");
+}
+
+catch
+{
+    Console.WriteLine("Введен неверный формат, попробуйте еще раз.");
+}
+
+
