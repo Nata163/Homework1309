20f51bd9332734d2c5e000e592c049b087fc7910
Nata <nata.9688@mail.ru>
2022-09-15 09:24:01 +0400

    Add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+
+int[] FillArray()
+{
+    int[] myArray = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+    return myArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+
+int[] array = FillArray();
+Console.WriteLine("Вывод массива: ");
+PrintArray(array);
